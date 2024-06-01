e943c375336558473cb3ac8bbe50c23933d4d796
Nishko <rowanander7@gmail.com>
2024-06-01 14:36:06 +1000

    adding set up files

---
+++ bot.js
@@ -0,0 +1,8 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('your-bot-token');
